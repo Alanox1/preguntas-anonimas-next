becedb0658a9be353b6e4e142a6318f9a8cd3655
Alan Magliolini <alannm54@gmail.com>
2024-05-07 23:57:30 -0300

    Sigo solucionando typescript

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -18,14 +18,25 @@ interface FirebaseQuestionData {
 export default function Home() {
   const [ questions, setQuestions ] = useState<QuestionsType[]>([])
 
-  console.log(questions)
-
   useEffect(() => {
     const querydb = getFirestore(app)
     const queryCollection = collection(querydb, "questions")
     getDocs(queryCollection)
     // .then(res => setQuestions(res.docs.map((question: { id: string; data: any }) => ({id : question.id, ...question.data()}))))
-    .then(res => setQuestions(res.docs.map((question: { id: string; data: any }) => ({id : question.id,question: (question.data() as FirebaseQuestionData).question}))))
+    // .then(res => setQuestions(res.docs.map((question: { id: string; data: any }) => ({id : question.id,question: (question.data() as FirebaseQuestionData).question}))))
+    .then((querySnapshot) => {
+      const fetchedQuestions: QuestionsType[] = [];
+      querySnapshot.forEach((doc) => {
+        const data = doc.data() as FirebaseQuestionData;
+        const question: QuestionsType = {
+          id: doc.id,
+          question: data.question,
+          // Otros campos si los hay
+        };
+        fetchedQuestions.push(question);
+      });
+      setQuestions(fetchedQuestions);
+    })
     .catch(err => {
       console.log(err)
     })
